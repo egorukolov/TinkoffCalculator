5a8f0ac20ef6063a72a21bb6df701c6c7e3045ee
Egor Ukolov <ukolovegor@yandex.ru>
2024-04-27 17:41:08 +0700

    Push

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Egor Ukolov on 27.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //
+    }
+    
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        guard let buttonText = sender.currentTitle else { return }
+        
+        print(buttonText)
+    }
+    
+
+}
+
